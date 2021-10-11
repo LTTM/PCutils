4a704489e63e8400f726cc8db3100c83a1a8596e
Daniele Mari <daniseas98@gmail.com>
2021-10-11 14:10:51 +0200

    [Add]file needed by pip to create the package

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='PCutils',
+    version='0.0.1',
+    author='Daniele Mari',
+    author_email='daniele.mari@phd.unipd.it',
+    description='Package with useful processing functions for PCs with focus on PC coding',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/LTTM/PCutils',
+    project_urls = {
+    },
+    license='MIT',
+    packages=['PCutils'],
+    install_requires=['numpy', 'matplotlib', "plyfile"],
+)
