d82d2b17aaa8d7850ceddcc958cebc7d4f05f713
Daniele Mari <daniseas98@gmail.com>
2021-10-12 11:11:51 +0200

    [Add]now it is possible to import directly from PCutils

--- PCutils/__init__.py
+++ PCutils/__init__.py
@@ -1,1 +1,5 @@
-
+from functions import flatten_cubes, unflatten_cubes, voxelize_PC
+from functions import qualityPCL, plot_pointcloud, read_ply_files
+from functions import write_ply_file, encode_with_TMC13, decode_with_TMC13
+from functions import decode_with_draco, decode, encode_with_draco
+from functions import encode, encode_and_decode, bj_delta 
