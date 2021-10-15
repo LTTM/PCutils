d1e24c1764df00c4934f9bbd858827028571ed81
Daniele Mari <daniseas98@gmail.com>
2021-10-15 16:04:28 +0200

    [Add]_encode_with_TMC13 to __init__.py

--- PCutils/__init__.py
+++ PCutils/__init__.py
@@ -3,4 +3,4 @@ from .functions import qualityPCL, plot_pointcloud, read_ply_files
 from .functions import write_ply_file, encode_with_TMC13, decode_with_TMC13
 from .functions import decode_with_draco, decode, encode_with_draco
 from .functions import encode, encode_and_decode, bj_delta 
-from .functions import collapse_duplicate_points
+from .functions import collapse_duplicate_points, _encode_with_TMC13
