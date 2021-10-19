a04a947d81d1b93d186b37b49565f63338d50391
Daniele Mari <daniseas98@gmail.com>
2021-10-19 11:46:38 +0200

    [Add]now can import plot_scatter_pcs

--- PCutils/__init__.py
+++ PCutils/__init__.py
@@ -2,5 +2,5 @@ from .functions import flatten_cubes, unflatten_cubes, voxelize_PC
 from .functions import qualityPCL, plot_pointcloud, read_ply_files
 from .functions import write_ply_file, encode_with_TMC13, decode_with_TMC13
 from .functions import decode_with_draco, decode, encode_with_draco
-from .functions import encode, encode_and_decode, bj_delta 
+from .functions import encode, encode_and_decode, bj_delta, plot_scatter_pcs
 from .functions import collapse_duplicate_points, _encode_with_TMC13
